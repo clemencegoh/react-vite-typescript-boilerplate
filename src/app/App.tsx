45381654a6d710b2b646556310be182ee7fafc4a
Clemence <clemence@gsgasset.com>
2024-06-02 17:58:04 +0800

    add absolute import config to vite config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,11 @@ import {QueryClientProvider} from "@tanstack/react-query";
 function App() {
     const queryClient = customHookQueryWithInterceptor();
 
-    return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <h1>Hello world! App main</h1>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
