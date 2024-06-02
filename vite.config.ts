62a7658a0fb4a23b6418eaa59ec7b963622f713c
Clemence <clemence@gsgasset.com>
2024-06-02 12:48:46 +0800

    small commit for manual chunking with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // manual chunk here as needed
+          // console.log(id);
+          // if (id.includes('src/nest1')) {
+          //   return 'nested1';
+          // } else if (id.includes('src/nest2')) {
+          //   return 'nested2';
+          // } else {
+          //   return 'index';
+          // }
+        },
+      },
+    },
+  },
 })
